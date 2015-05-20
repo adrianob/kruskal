6a4cc0931e114095951a76024052a7a27d681551
adriano benin <adriano.benin@gmail.com>
2015-05-19 23:07:12 -0300

    initial commit

---
+++ grafo.go
@@ -0,0 +1,142 @@
+//autor: Adriano Carniel Benin - 173464
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type AdjList [][]int
+type WList [][]int
+
+//sort interface
+func (w WList) Len() int {
+	return len(w)
+}
+
+func (w WList) Swap(i, j int) {
+	w[i], w[j] = w[j], w[i]
+}
+
+func (w WList) Less(i, j int) bool {
+	return w[i][2] < w[j][2]
+}
+
+//return index of str in slice, -1 if not found
+func findSlice(slice []string, str string) (i int) {
+	for i, v := range slice {
+		if v == str {
+			return i
+		}
+	}
+	return -1
+}
+
+func convertInput(lines []string) [][]int {
+	var vertices_list []int
+	var graph [][]int
+
+	for _, v := range lines {
+		if len(v) > 1 {
+			for _, v := range strings.Fields(v[1 : len(v)-1]) {
+				i, _ := strconv.Atoi(v)
+				vertices_list = append(vertices_list, i)
+			}
+			graph = append(graph, vertices_list)
+			vertices_list = nil
+		}
+	}
+
+	return graph
+}
+
+//read text file into a adjacency and weight arrays
+func read_graph() (adjacency_list AdjList, weight_list WList) {
+	content, err := ioutil.ReadFile("grafo.txt")
+
+	if err != nil {
+		fmt.Println("arquivo nao existe")
+		return
+	}
+
+	lines := strings.Split(string(content), "\n")
+
+	adjacency_list = convertInput(lines[0:findSlice(lines, "")])
+	weight_list = convertInput(lines[findSlice(lines, ""):len(lines)])
+
+	return adjacency_list, weight_list
+}
+
+func (a_list AdjList) Spanning() bool {
+
+	for i, v := range a_list {
+		for _, vertex := range a_list[i+1:] {
+			if !a_list.Connected(v[0], vertex[0]) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func DFSBool(a_list AdjList, v int, discovered []bool) []bool {
+	adj_list := a_list[v][1:len(a_list[v])] //get all  connected vertices
+
+	discovered[v] = true
+	for _, vertex := range adj_list {
+		if !discovered[vertex] {
+			DFSBool(a_list, vertex, discovered)
+		}
+	}
+
+	return discovered
+}
+
+func (a_list AdjList) DFS(v int) []bool {
+	discovered := make([]bool, len(a_list))
+
+	return DFSBool(a_list, v, discovered)
+
+}
+
+func (a_list AdjList) Connected(v1 int, v2 int) bool {
+	v1_connected := a_list.DFS(v1)
+	return v1_connected[v2]
+}
+
+//receives sorted weight list
+func (a_list AdjList) Kruskal(weight_list WList) AdjList {
+	var forest AdjList
+
+	//separate graph into forests of 1 vertex
+	for _, v := range a_list {
+		forest = append(forest, append(make([]int, 0), v[0]))
+	}
+
+	for _, v := range weight_list {
+		if forest.Spanning() {
+			return forest
+		}
+		if !forest.Connected(v[0], v[1]) {
+			forest[v[0]] = append(forest[v[0]], v[1])
+			forest[v[1]] = append(forest[v[1]], v[0])
+		}
+
+	}
+
+	return forest
+
+}
+
+func main() {
+	adjacency_list, weight_list := read_graph()
+	var spanning_tree AdjList
+
+	sort.Sort(weight_list)
+	spanning_tree = adjacency_list.Kruskal(weight_list)
+	fmt.Println(spanning_tree)
+
+}
