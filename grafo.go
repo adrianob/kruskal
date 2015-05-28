a5afe020badb55f45cb8bcb86c16c7415b561bf7
adriano benin <adriano.benin@gmail.com>
2015-05-28 14:52:05 -0300

    fix graph file format

--- grafo.go
+++ grafo.go
@@ -1,5 +1,12 @@
 //autor: Adriano Carniel Benin - 173464
 //executável fornecido para Mac OS X, rodar 'go build grafo.go' para compilar em outro sistema
+//ex de saida:
+//lista de adjacencia
+//[[0 1] [1 0 3] [2 5] [3 4 1] [4 3 5] [5 2 4]]
+//array de array de inteiros onde o primeiro numero de cada array indica o vértice e os números restantes os nodos adjacentes
+//lista de pesos
+//[[2 5 1] [3 4 1] [0 1 2] [1 3 2] [4 5 2]]
+//lista de pesos onde os primeiros 2 inteiros indicam os vertices conectados e o terceiro número indica o peso da aresta
 
 package main
 
@@ -8,6 +15,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -15,6 +23,11 @@ import (
 
 type AdjList [][]int
 type WList [][]int
+
+type Graph struct {
+	adjacency_list AdjList
+	weight_list    WList
+}
 
 //sort interface
 func (w WList) Len() int {
@@ -30,9 +43,9 @@ func (w WList) Less(i, j int) bool {
 }
 
 //return index of str in slice, -1 if not found
-func findSlice(slice []string, str string) (i int) {
+func findSlice(re *regexp.Regexp, slice []string) (i int) {
 	for i, v := range slice {
-		if v == str {
+		if (re.FindStringIndex(v) != nil) && i != 0 {
 			return i
 		}
 	}
@@ -57,8 +70,8 @@ func convertInput(lines []string) [][]int {
 	return graph
 }
 
-//read text file into a adjacency and weight arrays
-func read_graph() (adjacency_list AdjList, weight_list WList) {
+//read text file into a graph struct
+func read_graph() (g Graph) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Arquivo de dados: ")
 	text, _, _ := reader.ReadLine()
@@ -71,12 +84,16 @@ func read_graph() (adjacency_list AdjList, weight_list WList) {
 
 	lines := strings.Split(string(content), "\n")
 
-	adjacency_list = convertInput(lines[0:findSlice(lines, "")])
-	weight_list = convertInput(lines[findSlice(lines, ""):len(lines)])
+	//search for beggining of weight list
+	re := regexp.MustCompile("(?m)^\\($")
+	adjacency_list := convertInput(lines[0:findSlice(re, lines)])
+	weight_list := convertInput(lines[findSlice(re, lines):len(lines)])
 
-	return adjacency_list, weight_list
+	g = Graph{adjacency_list, weight_list}
+	return g
 }
 
+//check for connection with DFS
 func (a_list AdjList) ConnectedBool(v int, v2 int, discovered []bool) bool {
 	adj_list := a_list[v][1:len(a_list[v])] //get all  connected vertices
 
@@ -99,24 +116,28 @@ func (a_list AdjList) Connected(v int, v2 int) bool {
 	return a_list.ConnectedBool(v, v2, discovered)
 }
 
-func (a_list AdjList) Kruskal(weight_list WList) (forest AdjList, total_weight int) {
+func (g Graph) Kruskal() (spanning_tree Graph, total_weight int) {
+	var forest AdjList
+	var weight_list WList
 	total_weight = 0
 	inserted_vertices := 0
-	vertices_size := len(a_list)
+	vertices_size := len(g.adjacency_list)
 
-	sort.Sort(weight_list)
+	sort.Sort(g.weight_list)
 	//separate graph into forests of 1 vertex
-	for _, v := range a_list {
+	for _, v := range g.adjacency_list {
 		forest = append(forest, append(make([]int, 0), v[0]))
 	}
 
-	for _, v := range weight_list {
-		//if number of edges = vertices - 1 tree is spanning
+	for _, v := range g.weight_list {
+		//if number of edges = vertices - 1, tree is spanning
 		if inserted_vertices == (vertices_size - 1) {
-			return forest, total_weight
+			return Graph{forest, weight_list}, total_weight
 		}
 		if !forest.Connected(v[0], v[1]) {
 			total_weight += v[2]
+			weight_list = append(weight_list, v)
+			//insert both ends of vertice on adjacency list
 			forest[v[0]] = append(forest[v[0]], v[1])
 			forest[v[1]] = append(forest[v[1]], v[0])
 			inserted_vertices++
@@ -124,20 +145,22 @@ func (a_list AdjList) Kruskal(weight_list WList) (forest AdjList, total_weight i
 
 	}
 
-	return forest, total_weight
+	return Graph{forest, weight_list}, total_weight
 
 }
 
 func main() {
-	adjacency_list, weight_list := read_graph()
-	var spanning_tree AdjList
+	graph := read_graph()
+	var spanning_tree Graph
 	var total_weight int
 
-	spanning_tree, total_weight = adjacency_list.Kruskal(weight_list)
+	spanning_tree, total_weight = graph.Kruskal()
 	fmt.Println("arvore fornecida")
-	fmt.Println(adjacency_list)
-	fmt.Println("arvore geradora minima")
-	fmt.Println(spanning_tree)
+	fmt.Println(graph.adjacency_list)
+	fmt.Println("arvore geradora minima - lista de adjacência")
+	fmt.Println(spanning_tree.adjacency_list)
+	fmt.Println("arvore geradora minima - lista de pesos")
+	fmt.Println(spanning_tree.weight_list)
 	fmt.Println("peso total")
 	fmt.Println(total_weight)
 
